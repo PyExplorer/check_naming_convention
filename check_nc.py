bd14204dbc52163de0e8edd20bf839e02d40d1f0
taras <taras@scrapinghub.com>
2018-05-20 20:53:48 +0300

    refactoring main module, add logging

--- check_nc.py
+++ check_nc.py
@@ -1,28 +1,42 @@
+import logging
 from os import path as os_path
 
 from git import Repo
 from git import exc
 from git.util import rmtree
-from parser_args import parse_args
 
 from formatter import format_csv, format_json, format_txt
+from parser_args import parse_args
 from utils import (
     get_filenames_with_ext_in_path, get_functions_from_trees,
     get_top_words, get_trees, get_valid_names_from_nodes, get_vars_from_trees,
     get_words_from_node_name
 )
 
+tags = {
+    'verbs': 'VB',
+    'nouns': 'NN'
+}
+plang = {
+    'python': 'py'
+}
+node_handler = {
+    'func': get_functions_from_trees,
+    'var': get_vars_from_trees
+}
+format_handler = {
+    'txt': format_txt,
+    'json': format_json,
+    'csv': format_csv,
+}
+
 
 def clone_repository(repo, dest_dir):
     try:
         repo = Repo.clone_from(repo, os_path.join('.', dest_dir))
-
         return repo
     except exc.GitCommandError as e:
-        print(
-            'Something wrong happened, {} with status {}'.format(
-                e.stderr, e.status)
-        )
+        logging.error('Error with creating getting repo: {}'.format(e.stderr))
     return
 
 
@@ -50,47 +64,52 @@ def output_report(report, filename):
         f.write(report)
 
 
-def main():
-    tags = {
-        'verbs': 'VB',
-        'nouns': 'NN'
-    }
-    plang = {
-        'python': 'py'
-    }
-    node_handler = {
-        'func': get_functions_from_trees,
-        'var': get_vars_from_trees
-    }
-    format_handler = {
-        'txt': format_txt,
-        'json': format_json,
-        'csv': format_csv,
-    }
-
-    args = parse_args()
+def analyze_it(args):
     if args.noclone:
+        logging.info('Take repo from disk')
         working_dir = os_path.join('.', args.dir)
     else:
+        logging.info('Take repo from GitHub')
         repo = clone_repository(args.repo, args.dir)
         if not repo:
-            exit(1)
+            raise Exception
         working_dir = repo.working_dir
 
     if not os_path.exists(working_dir):
-        exit(1)
+        logging.error(
+            'Working directory doesn\'t exist {}'.format(working_dir)
+        )
+        raise Exception
 
     words = get_words_in_path(
         working_dir,
         tags[args.pos],
         node_handler[args.node],
+
         plang[args.plang]
     )
     top_words = get_top_words(words)
     report = create_report(top_words, format_handler[args.format])
     output_report(report, args.file.format())
     if args.clear:
+        logging.info('Remove repo from disk after finish')
         rmtree(working_dir)
+
+
+def main():
+    args = parse_args()
+    logging.basicConfig(filename=args.log, level=logging.INFO,
+                        format='[%(asctime)s] %(levelname).1s %(message)s',
+                        datefmt='%Y.%m.%d %H:%M:%S'
+                        )
+    logging.info('Start script')
+
+    try:
+        analyze_it(args)
+        logging.info('Work done')
+    except Exception as e:
+        logging.error('Something went wrong {}'.format(e))
+        logging.info('Script stopped with an error')
 
 
 if __name__ == '__main__':
